92e00ad91e035453390e8f542c3b6f11f640589a
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2023-10-26 08:35:09 +0530

    Update 4.tsp.cpp

--- DAAL/4.tsp.cpp
+++ DAAL/4.tsp.cpp
@@ -1,147 +1,171 @@
 #include <bits/stdc++.h>
+#define inf INT_MAX
 using namespace std;
 
-#define N 5
-#define INF INT_MAX
-
-class Node {
-public:
-    vector<pair<int, int>> path;
-    int reducedMatrix[N][N];
-    int totalCost;
-    int currentNode;
-    int currentLevel;
-};
-
-// Create a new node with updated data
-Node* createNode(int parentMatrix[N][N], vector<pair<int, int>> const& path, int level, int i, int j) {
-    Node* node = new Node;
-    node->path = path;
-    if (level != 0)
-        node->path.push_back(make_pair(i, j));
-    memcpy(node->reducedMatrix, parentMatrix, sizeof node->reducedMatrix);
-    for (int k = 0; level != 0 && k < N; k++) {
-        node->reducedMatrix[i][k] = INF;
-        node->reducedMatrix[k][j] = INF;
-    }
-    node->reducedMatrix[j][0] = INF;
-    node->currentLevel = level;
-    node->currentNode = j;
-    return node;
+void line(int l)
+{
+    for (int i = 0; i < l; i++)
+        cout << "_";
+    cout << endl;
 }
 
-// Reduce the rows of the matrix
-void reduceRows(int reducedMatrix[N][N], int row[N]) {
-    fill_n(row, N, INF);
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            if (reducedMatrix[i][j] < row[i])
-                row[i] = reducedMatrix[i][j];
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            if (reducedMatrix[i][j] != INF && row[i] != INF)
-                reducedMatrix[i][j] -= row[i];
-}
-
-// Reduce the columns of the matrix
-void reduceColumns(int reducedMatrix[N][N], int col[N]) {
-    fill_n(col, N, INF);
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            if (reducedMatrix[i][j] < col[j])
-                col[j] = reducedMatrix[i][j];
-
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            if (reducedMatrix[i][j] != INF && col[j] != INF)
-                reducedMatrix[i][j] -= col[j];
-}
-
-// Calculate the total cost of the reduced matrix
-int calculateCost(int reducedMatrix[N][N]) {
-    int cost = 0;
-    int row[N];
-    reduceRows(reducedMatrix, row);
-    int col[N];
-    reduceColumns(reducedMatrix, col);
-    for (int i = 0; i < N; i++)
-        cost += (row[i] != INT_MAX) ? row[i] : 0, cost += (col[i] != INT_MAX) ? col[i] : 0;
-    return cost;
-}
-
-// Print the path of the nodes
-void printPath(vector<pair<int, int>> const& path) {
-    for (int i = 0; i < path.size(); i++)
-        cout << path[i].first + 1 << " -> " << path[i].second + 1 << endl;
-}
-
-class Comparator {
-public:
-    bool operator()(const Node* lhs, const Node* rhs) const {
-        return lhs->totalCost > rhs->totalCost;
-    }
-};
-
-// Solve the problem using A* algorithm
-int solve(int adjacencyMatrix[N][N]) {
-    priority_queue<Node*, vector<Node*>, Comparator> pq;
-    vector<pair<int, int>> v;
-    Node* root = createNode(adjacencyMatrix, v, 0, -1, 0);
-    root->totalCost = calculateCost(root->reducedMatrix);
-    pq.push(root);
-    while (!pq.empty()) {
-        Node* minNode = pq.top();
-        pq.pop();
-        int i = minNode->currentNode;
-        if (minNode->currentLevel == N - 1) {
-            minNode->path.push_back(make_pair(i, 0));
-            cout << "Optimal Path: " << endl;
-            printPath(minNode->path);
-            return minNode->totalCost;
+void print_cost(vector<vector<int>> cost, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (cost[i][j] == inf)
+                cout << "X\t";
+            else
+                cout << cost[i][j] << "\t";
         }
-        for (int j = 0; j < N; j++) {
-            if (minNode->reducedMatrix[i][j] != INF) {
-                Node* child = createNode(minNode->reducedMatrix, minNode->path, minNode->currentLevel + 1, i, j);
-                child->totalCost = minNode->totalCost + minNode->reducedMatrix[i][j] + calculateCost(child->reducedMatrix);
-                pq.push(child);
+        cout << endl;
+    }
+}
+
+void fillinf(vector<vector<int>> &cost, vector<int> trav, int n, int s, int d)
+{
+    cost[d][s] = inf;
+    for (int t : trav)
+        cost[d][t] = inf;
+    for (int i = 0; i < n; i++)
+    {
+        cost[i][d] = inf;
+        cost[s][i] = inf;
+    }
+}
+
+int reduce(vector<vector<int>> &cost, int n)
+{
+    int r = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int row_min = cost[i][0];
+        for (int j = 1; j < n; j++)
+            row_min = min(cost[i][j], row_min);
+
+        if (row_min != inf)
+        {
+            r += row_min;
+            for (int j = 0; j < n; j++)
+                if (cost[i][j] != inf)
+                    cost[i][j] -= row_min;
+        }
+    }
+    for (int j = 0; j < n; j++)
+    {
+        int col_min = cost[0][j];
+        for (int i = 1; i < n; i++)
+            col_min = min(cost[i][j], col_min);
+
+        if (col_min != inf)
+        {
+            r += col_min;
+            for (int i = 0; i < n; i++)
+                if (cost[i][j] != inf)
+                    cost[i][j] -= col_min;
+        }
+    }
+    return r;
+}
+
+void tsp(vector<vector<int>> source, int n, int start)
+{
+
+    line(50);
+    cout << "\n\n\nTraveling Salesman Problem\nTotal locations = " << n << "\tStarting location = " << start << "\n\n\n";
+    line(50);
+    int s = start;
+    vector<int> trav;
+    int tcost = reduce(source, n);
+
+    while (trav.size() != n)
+    {
+        int d = s;
+        int min_cost = inf;
+        vector<vector<int>> new_source = source;
+        cout << "Current location = " << s << "\tCost = " << tcost << endl;
+        line(50);
+        print_cost(source, n);
+        line(50);
+        cout << "Possible next locations\n";
+        line(50);
+        for (int i = 0; i < n; i++)
+        {
+            if (i == s || count(trav.begin(), trav.end(), i))
+                continue;
+            vector<vector<int>> dest = source;
+            fillinf(dest, trav, n, s, i);
+            int r = reduce(dest, n);
+            int c = tcost + source[s][i] + r;
+            print_cost(dest, n);
+            cout << "\nLocation = " << i << "\tCost"
+                 << " = " << tcost << " + " << source[s][i] << " + " << r << " = " << c << endl;
+            line(50);
+            if (min_cost > c)
+            {
+                d = i;
+                min_cost = c;
+                new_source = dest;
             }
         }
-        delete minNode;
+
+        if (s == d)
+            d = start;
+        cout << "Traveled to location = " << d << "\tCost"
+             << " = " << tcost << endl;
+
+        line(50);
+        trav.push_back(s);
+        s = d;
+        if (min_cost != inf)
+            tcost = min_cost;
+        source = new_source;
+    }
+
+    cout << "Solution\nCost\t" << tcost << "\nPath\t";
+    for (int t : trav)
+        cout << t << " => ";
+    cout << start << endl;
+}
+
+int main()
+{
+    int n, start = 0;
+    cout << "Enter number of locations >>>";
+    cin >> n;
+    vector<vector<int>> source(n, vector<int>(n, inf));
+    cout << "Enter cost adjacency matrix\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i != j)
+                cin >> source[i][j];
+
+    while (start >= 0 && start < n)
+    {
+        line(50);
+        cout << "Enter starting location >>>";
+        cin >> start;
+        tsp(source, n, start);
     }
 }
 
-int main() {
-    int adjacencyMatrix[N][N];
-    
-    // Input adjacency matrix from the user
-    cout << "Enter the adjacency matrix (" << N << "x" << N << "):" << endl;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> adjacencyMatrix[i][j];
-        }
-    }
-
-    cout << "Optimal Cost: " << solve(adjacencyMatrix) << endl;
-
-    return 0;
-}
 
 
+// vector<vector<int>> c1 = {
+//     {inf, 20, 30, 10, 11},
+//     {15, inf, 16, 4, 2},
+//     {3, 5, inf, 2, 4},
+//     {19, 6, 18, inf, 3},
+//     {16, 4, 7, 16, inf}};
 
 
-// Enter the adjacency matrix (5x5):
-// INF 20 30 10 11
-// 15 INF 16 4 2
-// 3 5 INF 2 4
-// 19 6 18 INF 3
-// 16 4 7 16 INF
+//     20  30  10  11
+// 15      16  4   2
+// 3   5       2   4
+// 19  6   18      3
+// 16  4   7   16
 
-// Optimal Path:
-// 1 -> 4
-// 4 -> 2
-// 2 -> 5
-// 5 -> 3
-// Optimal Cost: 16
+// 0 3 1 4 2
+
