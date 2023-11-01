bed83ecfff536915df4e9165068ebb2b0d495a95
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2023-11-01 15:44:27 +0530

    6.pagereplace.c

--- OSL/6.pagereplace.c
+++ OSL/6.pagereplace.c
@@ -1,283 +1,168 @@
 #include <stdio.h>
 
-//Prototypes
-void fifo(int frameSize,int ref[],int n);
-void table(int frameSize,int refSize,int hitFlag[],int frame[][refSize]);
-void LRU(int frameSize,int ref[],int n);
-void optimal(int frameSize,int ref[],int n);
+int n, nf;
+int in[100];
+int p[50];
+int hit = 0;
+int i, j, k;
+int pgfaultcnt = 0;
 
-int main(){ 
-    printf("Enter the size of the reference String :: ");
-    int n; scanf("%d",&n);
+void getData() {
+    printf("Enter the length of the page reference sequence: ");
+    scanf("%d", &n);
+    printf("Enter the page reference sequence: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &in[i]);
+    printf("Enter the number of frames: ");
+    scanf("%d", &nf);
+}
 
-    int ref[n];
-    printf("Enter the reference string :: ");
-    for(int i = 0; i<n; i++){
-        scanf("%d",&ref[i]);
-    }
+void initialize() {
+    pgfaultcnt = 0;
+    for (i = 0; i < nf; i++)
+        p[i] = 9999;
+}
 
-    printf("Enter the frame size :: ");
-    int frameSize; scanf("%d",&frameSize);
-
-    int flag = 1; 
-    while(flag){
-        printf("Menu :: 1) FIFO\t2) LRU\t3) Optimal  4) Exit\nChoice :: ");
-        int ch; scanf("%d",&ch);
-        printf("\n");
-        switch (ch)
-        {
-            case 1:
-                fifo(frameSize,ref,n);
-                break;
-            case 2: 
-                LRU(frameSize,ref,n);
-                break;
-            case 3: 
-                optimal(frameSize,ref,n);
-                break;
-            case 4: 
-                flag = 0;
-                printf("\nExiting ...\n");
-                break;
-            default:
-                printf("Invalid Input !!!\n\n");
+int isHit(int data) {
+    hit = 0;
+    for (j = 0; j < nf; j++) {
+        if (p[j] == data) {
+            hit = 1;
             break;
         }
     }
+    return hit;
 }
 
-void fifo(int frameSize,int ref[],int n){
-
-    int frame[frameSize][n];
-
-    // Initializing the frame to 0    
-    for(int i = 0; i<frameSize; i++){
-        for(int j = 0; j<n; j++){
-            frame[i][j] = 0;
-        }
-    }
-    // Creating a flag array and initializing to 0
-    // 0 = MISS, 1 = HIT
-    int hitFlag[n];
-    for(int i = 0; i<n; i++){
-        hitFlag[i] = 0;
-    }
-
-    // Logic for FIFO
-    int j = 0;
-    for(int i = 0; i<n; i++){
-        int page = ref[i];
-        j = j%frameSize;
-        // Checking for PAGE FAULT
-        for(int k = 0; k<frameSize; k++){
-            if(i>0) frame[k][i] = frame[k][i-1];
-            if(page == frame[k][i]){
-                hitFlag[i] = 1; 
-                break;
-            }
-        }
-
-        // if hit else miss
-        if( hitFlag[i] == 0 ){
-            frame[j][i] = page;
-            j++;
-        }
-        else{
-            for (int k = 0; k<frameSize; k++){
-                frame[k][i] = frame[k][i-1];
-            }
-        }
-    }
-
-    table(frameSize,n,hitFlag,frame);
-}
-
-void LRU(int frameSize,int ref[],int n){
-
-    int frame[frameSize][n];
-    // Initializing the frame to 0    
-    for(int i = 0; i<frameSize; i++){
-        for(int j = 0; j<n; j++){
-            frame[i][j] = 0;
-        }
-    }
-    // Creating a flag array and initializing to 0
-    // 0 = MISS, 1 = HIT
-    int hitFlag[n];
-    for(int i = 0; i<n; i++){
-        hitFlag[i] = 0;
-    }
-
-    int cnt = 0; 
-    for(int i = 0; i<n; i++){
-        int page = ref[i];
-        // Checking for PAGE FAULT
-        for(int k = 0; k<frameSize; k++){
-            if(i>0) frame[k][i] = frame[k][i-1];
-            if(page == frame[k][i]){
-                hitFlag[i] = 1; 
-                break;
-            }
-        }
-
-        if(hitFlag[i] == 0){
-            // Miss Case
-            if(cnt < frameSize){
-                frame[cnt][i] = page;
-                cnt++;
-            }
-            else{
-                int f[frameSize];
-                for(int i = 0; i<frameSize; i++) f[i] = 0; 
-   
-                /*storing the indexes of the frame elements to find the farthest element*/
-                for(int k = 0; k<frameSize; k++){
-                    int frameElement = frame[k][i];
-                    for(int j = i-1; j>=0; j--){
-                        if(ref[j] == frameElement){
-                            f[k] = j; 
-                            break;
-                        }
-                    }
-                }
-
-                //Finding the farthest frame 
-                int miniIndex = 1e9;
-                int fReplace = -1;
-                for(int k = 0; k<frameSize; k++){
-                    if(f[k] < miniIndex){
-                        miniIndex = f[k];
-                        fReplace = k; 
-                    }
-                }
-
-                // frame replaced
-                // printf("%d-F\n",fReplace);
-                frame[fReplace][i] = page;
-            }
-        }
-        else{
-            // Hit Case
-            for (int k = 0; k<frameSize; k++){
-                frame[k][i] = frame[k][i-1];
-            }
-        }
-    }
-    // Print the table
-    table(frameSize,n,hitFlag,frame);
-}
-
-void optimal(int frameSize,int ref[],int n){
-    int frame[frameSize][n];
-    // Initializing the frame to 0    
-    for(int i = 0; i<frameSize; i++){
-        for(int j = 0; j<n; j++){
-            frame[i][j] = 0;
-        }
-    }
-    // Creating a flag array and initializing to 0
-    // 0 = MISS, 1 = HIT
-    int hitFlag[n];
-    for(int i = 0; i<n; i++){
-        hitFlag[i] = 0;
-    }
-
-    int cnt = 0; 
-    for(int i = 0; i<n; i++){
-        int page = ref[i];
-        // Checking for PAGE FAULT
-        for(int k = 0; k<frameSize; k++){
-            if(i>0) frame[k][i] = frame[k][i-1];
-            if(page == frame[k][i]){
-                hitFlag[i] = 1; 
-                break;
-            }
-        }
-
-        if(hitFlag[i] == 0){
-            // Miss Case
-            if(cnt < frameSize){
-                frame[cnt][i] = page;
-                cnt++;
-            }
-            else{
-                int f[frameSize];
-                for(int i = 0; i<frameSize; i++) f[i] = 0; 
-   
-                /*storing the indexes of the frame elements to find the farthest element*/
-                for(int k = 0; k<frameSize; k++){
-                    int frameElement = frame[k][i];
-                    for(int j = i+1; j<n; j++){
-                        if(ref[j] == frameElement){
-                            f[k] = j; 
-                            break;
-                        }
-                    }
-                }
-
-                //Finding the farthest frame 
-                int maxIndex = -1e9;
-                int fReplace = -1;
-                for(int k = 0; k<frameSize; k++){
-                    if(f[k] > maxIndex){
-                        maxIndex = f[k];
-                        fReplace = k; 
-                    }
-                }
-
-                // frame replaced
-                // printf("%d-F\n",fReplace);
-                frame[fReplace][i] = page;
-            }
-        }
-        else{
-            // Hit Case
-            for (int k = 0; k<frameSize; k++){
-                frame[k][i] = frame[k][i-1];
-            }
-        }
-    }
-    // Print the table
-    table(frameSize,n,hitFlag,frame);
-}
-
-void table(int frameSize,int refSize, int hitFlag[],int frame[][refSize]){
-
-    int hitCnt = 0; 
-    int missCnt = 0; 
-
-    for (int i = 0; i < refSize; i++){
-        printf("-------");
-    }
-    printf("\n|");
-    for(int i = 0; i<refSize; i++){
-        if(hitFlag[i]){
-            printf(" HIT  |");
-            hitCnt++;
-        }
-        else{
-            printf(" MISS |");
-            missCnt++;
-        } 
+void dispPages(char *algoName) {
+    printf("Current Pages in Memory for %s: ", algoName);
+    for (k = 0; k < nf; k++) {
+        if (p[k] != 9999)
+            printf("%d ", p[k]);
     }
     printf("\n");
-    for (int i = 0; i < refSize; i++){
-        printf("-------");
-    }
-    printf("\n");
+}
 
-    for(int j = 0; j<frameSize; j++){
-        printf("|");
-        for(int i = 0; i<refSize; i++){
-            printf("   %d  |",frame[j][i]);
+void dispPgFaultCnt(int pageFaults, char *algoName) {
+    printf("Total number of page faults for %s: %d\n", algoName, pageFaults);
+}
+
+void fifo() {
+    char algoName[] = "FIFO";
+    initialize();
+    for (i = 0; i < n; i++) {
+        printf("For page %d using %s: ", in[i], algoName);
+        if (isHit(in[i]) == 0) {
+            for (k = 0; k < nf - 1; k++)
+                p[k] = p[k + 1];
+            p[k] = in[i];
+            pgfaultcnt++;
+            dispPages(algoName);
+        } else
+            printf("No page fault\n");
+    }
+    dispPgFaultCnt(pgfaultcnt, algoName);
+}
+
+void optimal() {
+    char algoName[] = "Optimal";
+    initialize();
+    int near[50];
+    for (i = 0; i < n; i++) {
+        printf("For page %d using %s: ", in[i], algoName);
+        if (isHit(in[i]) == 0) {
+            for (j = 0; j < nf; j++) {
+                int pg = p[j];
+                int found = 0;
+                for (k = i; k < n; k++) {
+                    if (pg == in[k]) {
+                        near[j] = k;
+                        found = 1;
+                        break;
+                    } else
+                        found = 0;
+                }
+                if (!found)
+                    near[j] = 9999;
+            }
+            int max = -9999;
+            int repindex;
+            for (j = 0; j < nf; j++) {
+                if (near[j] > max) {
+                    max = near[j];
+                    repindex = j;
+                }
+            }
+            p[repindex] = in[i];
+            pgfaultcnt++;
+            dispPages(algoName);
+        } else
+            printf("No page fault\n");
+    }
+    dispPgFaultCnt(pgfaultcnt, algoName);
+}
+
+void lru() {
+    char algoName[] = "LRU";
+    initialize();
+    int least[50];
+    for (i = 0; i < n; i++) {
+        printf("For page %d using %s: ", in[i], algoName);
+        if (isHit(in[i]) == 0) {
+            for (j = 0; j < nf; j++) {
+                int pg = p[j];
+                int found = 0;
+                for (k = i - 1; k >= 0; k--) {
+                    if (pg == in[k]) {
+                        least[j] = k;
+                        found = 1;
+                        break;
+                    } else
+                        found = 0;
+                }
+                if (!found)
+                    least[j] = -9999;
+            }
+            int min = 9999;
+            int repindex;
+            for (j = 0; j < nf; j++) {
+                if (least[j] < min) {
+                    min = least[j];
+                    repindex = j;
+                }
+            }
+            p[repindex] = in[i];
+            pgfaultcnt++;
+            dispPages(algoName);
+        } else
+            printf("No page fault\n");
+    }
+    dispPgFaultCnt(pgfaultcnt, algoName);
+}
+
+int main() {
+    int choice;
+    while (1) {
+        printf("Page Replacement Algorithms\n");
+        printf("1. Enter data\n2. FIFO\n3. Optimal\n4. LRU\n5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                getData();
+                break;
+            case 2:
+                fifo();
+                break;
+            case 3:
+                optimal();
+                break;
+            case 4:
+                lru();
+                break;
+            default:
+                return 0;
+                break;
         }
-        printf("\n");
     }
-
-    for (int i = 0; i < refSize; i++){
-        printf("-------");
-    }
-    printf("\n\n");
-    printf("Hit  Percent :: %0.2f\n", (float) hitCnt/refSize * 100);
-    printf("Miss Percent :: %0.2f\n\n", (float) missCnt/refSize * 100);
 }
