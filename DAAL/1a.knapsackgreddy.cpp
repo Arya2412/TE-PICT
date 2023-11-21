423d65c05f9523d480a687b9630d92ecbbd2f0ed
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2023-11-21 11:05:48 +0530

    Donee

--- DAAL/1a.knapsackgreddy.cpp
+++ DAAL/1a.knapsackgreddy.cpp
@@ -16,7 +16,7 @@ bool cmp(Item a, Item b) {
     return a.ratio > b.ratio;
 }
 
-// Function to solve Fractional Knapsack using greedy approach
+// Function to solve Fractional Knapsack using a greedy approach
 double fractionalKnapsack(vector<Item>& items, int cap) {
     sort(items.begin(), items.end(), cmp); // Sort items by value-to-weight ratio
 
@@ -24,20 +24,31 @@ double fractionalKnapsack(vector<Item>& items, int cap) {
     int n = items.size();
 
     cout << "Fractional Knapsack greedy approach:" << endl;
-    cout << "Item  Weight  Value  Fraction  Profit" << endl;
+    cout << setfill('-') << setw(58) << "" << setfill(' ') << endl;
+    cout << "| " << setw(4) << "Item" << " | " << setw(7) << "Weight" << " | " << setw(7) << "Value" << " | " << setw(11) << "Fraction" << " | " << setw(8) << "Profit" << " | " << setw(18) << "Selection Status" << " |" << endl;
+    cout << setfill('-') << setw(58) << "" << setfill(' ') << endl;
 
     for (int i = 0; i < n && cap > 0; i++) {
+        string selectionStatus = "";
         if (cap >= items[i].weight) {
             double fraction = 1.0; // Select the whole item
-            cout << setw(4) << i + 1 << setw(7) << items[i].weight << setw(7) << items[i].value << setw(10) << fraction << setw(9) << items[i].value << endl;
+            selectionStatus = "Selected Completely";
+            cout << "| " << setw(4) << i + 1 << " | " << setw(7) << items[i].weight << " | " << setw(7) << items[i].value << " | " << setw(10) << fraction << " | " << setw(9) << items[i].value << " | " << setw(18) << selectionStatus << " |" << endl;
             profit += items[i].value;
             cap -= items[i].weight;
         } else {
             double fraction = (double)cap / items[i].weight; // Select a fraction of the item
-            cout << setw(4) << i + 1 << setw(7) << items[i].weight << setw(7) << items[i].value << setw(10) << fraction << setw(9) << fraction * items[i].value << endl;
+            selectionStatus = "Selected Partially";
+            cout << "| " << setw(4) << i + 1 << " | " << setw(7) << items[i].weight << " | " << setw(7) << items[i].value << " | " << setw(10) << fraction << " | " << setw(9) << fraction * items[i].value << " | " << setw(18) << selectionStatus << " |" << endl;
             profit += fraction * items[i].value;
             cap = 0;
         }
+    }
+
+    cout << setfill('-') << setw(58) << "" << setfill(' ') << endl;
+
+    if (cap == 0) {
+        cout << "The bag is full." << endl;
     }
 
     return profit;
@@ -54,11 +65,16 @@ int main() {
 
     cout << "Enter weight and value of each item:" << endl;
     for (int i = 0; i < n; i++) {
-        cin >> items[i].weight >> items[i].value;
+        cout << "Item " << i + 1 << ":" << endl;
+        cout << "Weight: ";
+        cin >> items[i].weight;
+        cout << "Value: ";
+        cin >> items[i].value;
         items[i].ratio = (double)items[i].value / items[i].weight; // Calculate value-to-weight ratio
     }
 
     double maxProfit = fractionalKnapsack(items, cap);
+
     cout << "Maximum possible profit: " << maxProfit << endl;
 
     return 0;
